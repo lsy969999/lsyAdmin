d4669bb55f1fa9e07bd3b6c39610ae79c8691ba0
SY L <syl@SYui-MacBookPro.local>
2023-09-26 22:04:48 +0900

    layout header sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from '../../component/Header'
+import Sidebar from '../../component/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className=''>
+          {/* Sidebar Start */}
+          <Sidebar/>
+          {/* Sidebar End */}
+
+          {/* Content Area Start */}
+          <div>
+            {/* Header Start */}
+            <Header/>
+            {/* Header End */}
+
+            {/* Main Content Start */}
+            <main>
+              {children}
+            </main>
+            {/* Main Content End */}
+          </div>
+          {/* Content Area End */}
+        </div>
+      </body>
     </html>
   )
 }
